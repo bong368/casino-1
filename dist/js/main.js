59417a4dbd7b193728222a6eac8f67c48b5f0eb1
DuyPH <DuyPH>
2016-05-17 16:29:58 +0700

    update file

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,11 @@
 (function($) {
 	"use strict";
 	$(document).ready(function() {
-		
+		$("#frm-login").validate({
+			rules: {
+				username: "required",
+				password: "required"
+			}
+		});
 	});
 })(jQuery);
