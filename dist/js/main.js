d28edb734dba7bb497c9a2bec789ff27037bc88e
System Administrator <root@HD-2.local>
2016-06-14 02:03:07 +0700

    make xoc-dia

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (function($) {
 	"use strict";
 	$(document).ready(function() {
@@ -70,20 +69,39 @@
     if($('.owl-carousel-home').length != 0){
       $('.owl-carousel-home').owlCarousel({
         loop:true,
-        margin:100,
+        margin:90,
         nav:true,
         responsive:{
-            0:{
-                items:3
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:3
-            }
+          0:{
+              items:3
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:3
+          }
         }
-    })
+      })
+    }
+
+    if($('.owl-carousel-icongame').length != 0){
+      $('.owl-carousel-icongame').owlCarousel({
+        loop:true,
+        margin:90,
+        nav:true,
+        responsive:{
+          0:{
+              items:3
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:3
+          }
+        }
+      })
     }
 
     if($('.float-button').length != 0){
@@ -96,78 +114,3 @@
     });
 	});
 })(jQuery);
-=======
-(function($) {
-	"use strict";
-	$(document).ready(function() {
-		$("#frm-login").validate({
-			rules: {
-				username: "required",
-				password: "required"
-			}
-		});
-
-    if($('ul.tabs').length != 0){
-			jQuery('ul.tabs').each(function(){
-		    // For each set of tabs, we want to keep track of
-		    // which tab is active and it's associated content
-		    var $active, $content, $links = jQuery(this).find('a');
-
-		    // If the location.hash matches one of the links, use that as the active tab.
-		    // If no match is found, use the first link as the initial active tab.
-		    $active = jQuery($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
-		    $active.addClass('active');
-
-		    $content = $($active[0].hash);
-
-		    // Hide the remaining content
-		    $links.not($active).each(function () {
-	        jQuery(this.hash).hide();
-		    });
-
-		    // Bind the click event handler
-		    jQuery(this).on('click', 'a', function(e){
-	        // Make the old tab inactive.
-	        $active.removeClass('active');
-	        $content.hide();
-
-	        // Update the variables with the new link and content
-	        $active = jQuery(this);
-	        $content = jQuery(this.hash);
-
-	        // Make the tab active.
-	        $active.addClass('active');
-	        $content.show();
-
-	        // Prevent the anchor's default click action
-	        e.preventDefault();
-		    });
-			});
-    }
-
-		if($('.owl-carousel').length != 0){
-			$('.owl-carousel').owlCarousel({
-		    loop:true,
-		    autoplay: true,
-		    margin: 20,
-		    nav: true,
-		    responsive:{
-	        0:{
-	            items:1
-	        },
-	        600:{
-	            items:5
-	        },
-	        1000:{
-	            items:9
-	        }
-		    }
-			})
-		}
-
-		if($('.float-button').length !=0){
-			$('.float-button').draggable();
-		}
-	});
-})(jQuery);
->>>>>>> c942831be59720bc6ef846a2a9e51855d1e49782
